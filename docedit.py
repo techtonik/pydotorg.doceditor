38ad537f360bdcc5f929d01510510ea8f479bb86
anatoly techtonik <techtonik@gmail.com>
2011-03-31 13:29:26 +0300

    add 'command line framework' to keep actions away from coupling with web-specific implementation details

--- docedit.py
+++ docedit.py
@@ -3,6 +3,9 @@ Online documentation editor and patch generator.
 
 tracking issue: http://code.google.com/p/pydotorg/issues/detail?id=6
 online editor used for Google Code: http://codemirror.net/
+
+link to send Python documentation patches:
+http://bugs.python.org/issue?@template=item&components=4
 """
 
 #import webbrowser
@@ -18,3 +21,17 @@ def edit(filename=None):
   #  . show diff for copy/paste
 
   pass
+
+
+if __name__ == '__main__':
+  # -- command line framefork --
+  # 
+  # this framework allows to inspect available actions
+  # and test them from command line
+  #
+  actions = [a for a in dir() if not a.startswith('_')]
+  if actions:
+    print "Available actions:"
+    for a in actions:
+      print "- %s" % a
+      # [ ] print definition / prototype
