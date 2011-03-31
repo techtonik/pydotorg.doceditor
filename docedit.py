03b8a0928611fcba1fd03a8dc87feda023962e72
anatoly techtonik <techtonik@gmail.com>
2011-03-31 12:39:35 +0300

    idea prototype

---
+++ docedit.py
@@ -0,0 +1,13 @@
+#import webbrowser
+#import webserver
+
+def edit(filename=None):
+  # . GET
+  #  . if filename is not available - show list of files
+  #  . if available - show edit form
+  # . POST
+  #  . check params (invalid filename, wrong data)
+  #  . calculate diff
+  #  . show diff for copy/paste
+
+  pass
