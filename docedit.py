762f8c0f198b1dda2105d0e6eeacd0246cef4dbf
anatoly techtonik <techtonik@gmail.com>
2011-03-31 13:19:23 +0300

    description and links

--- docedit.py
+++ docedit.py
@@ -1,10 +1,17 @@
+"""
+Online documentation editor and patch generator.
+
+tracking issue: http://code.google.com/p/pydotorg/issues/detail?id=6
+online editor used for Google Code: http://codemirror.net/
+"""
+
 #import webbrowser
 #import webserver
 
 def edit(filename=None):
   # . GET
   #  . if filename is not available - show list of files
-  #  . if available - show edit form
+  #  . if available - show edit form, load file from URL
   # . POST
   #  . check params (invalid filename, wrong data)
   #  . calculate diff
