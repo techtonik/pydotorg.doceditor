8a2856bb662842966e4df65987489c4f0f88139c
anatoly techtonik <techtonik@gmail.com>
2011-03-31 20:12:39 +0300

    webapp: add urlmap and DocEdit handler (although no docedit import yet)

--- webui-webapp/main.py
+++ webui-webapp/main.py
@@ -6,13 +6,24 @@ from google.appengine.ext import webapp
 from google.appengine.ext.webapp.util import run_wsgi_app
 
 
-class MainHandler(webapp.RequestHandler):
+class MainPage(webapp.RequestHandler):
     def get(self):
-        self.response.out.write('Hello world!')
+        self.response.out.write('<a href="/docedit">DocEdit Version 0.1</a>')
+        # [ ] make this link to action through function
+        #     to avoid grepping when link changes
 
+class DocEdit(webapp.RequestHandler):
+    def get(self):
+        self.response.out.write('Still writing..')
+
+
+urlmap = [
+    ('/', MainPage),
+    ('/docedit', DocEdit),
+]
 
 def main():
-    app = webapp.WSGIApplication([('/', MainHandler)], debug=True)
+    app = webapp.WSGIApplication(urlmap, debug=True)
     run_wsgi_app(app)
 
 
